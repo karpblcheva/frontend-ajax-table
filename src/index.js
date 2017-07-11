05032eca5f9cc2a2226718a96340a7748c4c7fb2
karpblcheva <m.karpblcheva@gmail.com>
2017-07-11 15:51:08 +0300

    create 3rd and 4th tables

--- src/index.js
+++ src/index.js
@@ -2,41 +2,57 @@ axios.get('http://www.mocky.io/v2/55f748b33568195d044b3dc8')
   .then((response) => {
     const persons = response.data;
 
+    function renderTableBody(persons, tbody) {
+      persons.forEach((person) => {
+        const fullName = `${person.name.first} ${person.name.last}`;
+        const pictureUrl = person.picture;
+        const isActive = person.isActive;
+        const age = person.age;
 
-    persons.forEach((person) => {
-      const fullName = `${person.name.first} ${person.name.last}`;
-      const pictureUrl = person.picture;
-      const isActive = person.isActive;
-      $('.all-persons').append(`<tr>
-        <td><img src="${pictureUrl}"></td>
-        <td>${fullName}</td>
-        <td>${isActive}</td>
-        <td>${person.about}</td>
-        <td>${person.balance}</td>
-        <td>${person.age}</td>
-        <td>${person.registered}</td>
-        <td>${person.company}</td>
-        <td><a href="mailto:${person.email}">${person.email}</div></td>
-        <td><a href="tel:${person.phone}">${person.phone}</a></td>
-        <td>${person.address}</td>
-        </tr>`);
+        function showData(tbody) {
+          return tbody.append(`<tr>
+          <td><img src="${pictureUrl}"></td>
+          <td>${fullName}</td>
+          <td>${isActive}</td>
+          <td>${person.about}</td>
+          <td>${person.balance}</td>
+          <td>${age}</td>
+          <td>${person.registered}</td>
+          <td>${person.company}</td>
+          <td><a href="mailto:${person.email}">${person.email}</div></td>
+          <td><a href="tel:${person.phone}">${person.phone}</a></td>
+          <td>${person.address}</td>
+          </tr>`);
+        }
 
-      if (isActive == true) {
-        $('.active-persons').append(`<tr>
-        <td><img src="${pictureUrl}"></td>
-        <td>${fullName}</td>
-        <td>${isActive}</td>
-        <td>${person.about}</td>
-        <td>${person.balance}</td>
-        <td>${person.age}</td>
-        <td>${person.registered}</td>
-        <td>${person.company}</td>
-        <td><a href="mailto:${person.email}">${person.email}</div></td>
-        <td><a href="tel:${person.phone}">${person.phone}</a></td>
-        <td>${person.address}</td>
-        </tr>`);
+        showData(tbody);
+
+      });
+    }
+
+    renderTableBody(persons, $('.all-persons'));
+
+    // persons.filter(function (e) {
+    //   return e.isActive === true;
+    // });
+    // renderTableBody(persons, $('.active-persons'));
+
+    persons.sort(function (a, b) {
+      return a.age - b.age;
+    })
+    renderTableBody(persons, $('.sortByAge-persons'));
+
+    persons.sort(function (a, b) {
+      if (a.name.first < b.name.first) {
+        return -1;
       }
-    });
+      if (a.name.first > b.name.first) {
+        return 1;
+      }
+      return 0;
+    })
+    renderTableBody(persons, $('.sortByName-persons'));
+
   })
   .catch((error) => {
     console.log(error);
