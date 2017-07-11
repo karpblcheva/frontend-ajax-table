e65ac99d0ef7b0f26550b23138184bff98dac5f3
karpblcheva <m.karpblcheva@gmail.com>
2017-07-11 16:05:46 +0300

    create 5th table

--- src/index.js
+++ src/index.js
@@ -3,39 +3,40 @@ axios.get('http://www.mocky.io/v2/55f748b33568195d044b3dc8')
     const persons = response.data;
 
     function renderTableBody(persons, tbody) {
-      persons.forEach((person) => {
-        const fullName = `${person.name.first} ${person.name.last}`;
-        const pictureUrl = person.picture;
-        const isActive = person.isActive;
-        const age = person.age;
+      persons.forEach(
+        (person) => {
+          const fullName = `${person.name.first} ${person.name.last}`;
+          const pictureUrl = person.picture;
+          const isActive = person.isActive;
+          const age = person.age;
 
-        function showData(tbody) {
-          return tbody.append(`<tr>
-          <td><img src="${pictureUrl}"></td>
-          <td>${fullName}</td>
-          <td>${isActive}</td>
-          <td>${person.about}</td>
-          <td>${person.balance}</td>
-          <td>${age}</td>
-          <td>${person.registered}</td>
-          <td>${person.company}</td>
-          <td><a href="mailto:${person.email}">${person.email}</div></td>
-          <td><a href="tel:${person.phone}">${person.phone}</a></td>
-          <td>${person.address}</td>
-          </tr>`);
+          function showData(tbody) {
+            return tbody.append(`<tr>
+            <td><img src="${pictureUrl}"></td>
+            <td>${fullName}</td>
+            <td>${isActive}</td>
+            <td>${person.about}</td>
+            <td>${person.balance}</td>
+            <td>${age}</td>
+            <td>${person.registered}</td>
+            <td>${person.company}</td>
+            <td><a href="mailto:${person.email}">${person.email}</div></td>
+            <td><a href="tel:${person.phone}">${person.phone}</a></td>
+            <td>${person.address}</td>
+            </tr>`);
+          }
+
+          showData(tbody);
         }
-
-        showData(tbody);
-
-      });
-    }
+      );
+    };
 
     renderTableBody(persons, $('.all-persons'));
 
-    // persons.filter(function (e) {
-    //   return e.isActive === true;
-    // });
-    // renderTableBody(persons, $('.active-persons'));
+    let activePersons = persons.filter(function (a) {
+      return a.isActive === true;      
+    })
+    renderTableBody(activePersons, $('.active-persons'));
 
     persons.sort(function (a, b) {
       return a.age - b.age;
@@ -52,6 +53,11 @@ axios.get('http://www.mocky.io/v2/55f748b33568195d044b3dc8')
       return 0;
     })
     renderTableBody(persons, $('.sortByName-persons'));
+
+    let filteredPersons = persons.filter(function (a) {
+      return a.name.last.length >= 6;      
+    })
+    renderTableBody(filteredPersons, $('.filterByLastName-persons'));
 
   })
   .catch((error) => {
